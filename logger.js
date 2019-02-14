ff7ca6c78b5fd1b14994e5ddc675e44135b2acf3
Jcarrasco <no@email.com>
2019-02-14 21:51:27 +0100

    First attempt

---
+++ logger.js
@@ -0,0 +1,2 @@
+// Crear un modulo que logee en función del status de debug del .env con la hora y toda la info necesaria.
+// Este módulo se exporta aquí y se importa donde se quiera usar para que imprima por pantalla la info requerida
--- webcheck.js
+++ webcheck.js
@@ -4,54 +4,126 @@
 require('dotenv').config;
 
 const http = require('http');
+const { URL } = require('url');
+const querystring = require('querystring');
+
+const dotenv = require('dotenv');
+
 const cheerio = require('cheerio');
 const nodemailer = require('nodemailer');
 const moment = require('moment');
+const _ = require('underscore');
+
+const ENV = dotenv.config().parsed;
+
+_.each(ENV, (v, k) => {
+  if (v === 'true') {
+    ENV[k] = true;
+  } else if (v === 'false') {
+    ENV[k] = false;
+  } else if (v.indexOf('JSON@') !== -1){
+    console.log(v.split('JSON@')[1]);
+    ENV[k] = JSON.parse(v.split('JSON@')[1]);
+  }
+})
+
+const HTTP_GET = 'GET',
+  HTTP_POST = 'POST';
+
 
 let transporter = nodemailer.createTransport({
-  service: '',
+  service: ENV.MAIL_SERVICE,
   auth: {
-    user: '',
-    pass: ''
+    user: ENV.MAIL_ADDRESS,
+    pass: ENV.MAIL_PWD
   }
 });
 
 let mailOptions = {
-  from: '',
-  to: '',
-  subject: '',
-  text: 'http://www.elnautico.org/?p=2938'
+  from: ENV.MAIL_ADDRESS,
+  to: ENV.MAIL_RCVRS_LIST,
+  subject: ENV.MAIL_SUBJECT,
+  text: 'SUVIEJA'
 };
 
-http.get('http://www.elnautico.org/?cat=2', (res) => {
-  let $;
-  let divContentLen;
-  let doc = '';
+let doc = '';
 
-  res.on('error', (err) => {
-    console.log(`Error: ${err.message}`);
-  })
+switch (ENV.HTTP_METHOD) {
+  case HTTP_GET:
+    http.get(ENV.REQ_URL, (res) => {
 
-  res.on('data', (chunk) => {
-    doc += chunk;
-  })
-
-  res.on('end', () => {
-    console.log(`[${moment().format()}]    Checking web for changes...`);
-    $ = cheerio.load(doc);
-    divContentLen = $('a:contains("LEIVA vs. FERREIRO – 15/08/2018")')
-      .parent().parent().parent().children('div.entry-content')
-      .children().length
-    if (divContentLen != 3){
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err){
-          console.log(`[${moment().format()}]    error sending mail:: ${err}`);
-        } else {
-          console.log(`[${moment().format()}]    ENTRADAS?! - email sent`);
-        }
+      res.on('error', (err) => {
+        console.log(`Error: ${err.message}\n${err.stack}`);
       })
-    } else {
-      console.log(`[${moment().format()}]    No changes`);
-    }
-  })
-})
+
+      res.on('data', (chunk) => {
+        doc += chunk;
+      })
+
+      res.on('end', () => {
+        console.log(`Ended request. Obtained:\n${doc}`);
+        
+      })
+    })
+    break;
+  
+  case HTTP_POST:
+    const reqBody = ENV.REQ_DATA;
+    const targetUrl = new URL(ENV.REQ_URL);
+    
+    const options = {
+      hostname: targetUrl.hostname,
+      port: 80,
+      path: targetUrl.pathname,
+      method: 'POST',
+      headers: {
+        "Host": 'www3.emvs.es',
+        "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:64.0) Gecko/20100101 Firefox/64.0',
+        "Accept": '*/*',
+        "Accept-Language": 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+        "Accept-Encoding": 'gzip, deflate, br',
+        "Referer": 'https://www3.emvs.es/smaweb/',
+        "X-Requested-With": 'XMLHttpRequest',
+        "X-MicrosoftAjax": 'Delta=true',
+        "Cache-Control": 'no-cache',
+        "Content-Type": 'application/x-www-form-urlencoded; charset=utf-8',
+        "Content-Length": '2824',
+        "DNT": '1',
+        "Connection": 'keep-alive',
+        "Cookie": 'ASP.NET_SessionId=td3qi0m2o5gxg0m1oi4u3pii',
+        "Pragma": 'no-cache'
+      }
+    };
+
+    const req = http.request(options, (res) => {
+      ENV.DEBUG && console.log(`STATUS: ${res.statusCode}`);
+      ENV.DEBUG && console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        doc += chunk;
+      });
+      res.on('end', () => {
+        ENV.DEBUG && console.log(`DATA:\n${doc}`);
+        mailOptions.text = doc;        
+        ENV.MAIL_ENABLE && transporter.sendMail(mailOptions, (err, info) => {
+          if (err){
+            console.log(`[${moment().format()}]    error sending mail:: ${err}`);
+          } else {
+            console.log(`[${moment().format()}]    ENTRADAS?! - email sent`);
+          }
+        })
+      });
+    });
+    
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+    });
+
+    req.write(reqBody);
+    req.end();
+
+    break;
+
+  default:
+    break;
+}
